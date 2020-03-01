ffe67418ab3b745416f129468b3b6beb52b9b557
lievin-kim <lievin.kim@gmail.com>
2020-03-02 01:39:36 +0900

    Updates

---
+++ precache-manifest.478eb83fcd0614d29f0dc5a1a754c94a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8f11d709c3478d340c3b32f18a34da",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "098fa475eabab2caaaf9",
+    "url": "/movie_app_2020/static/css/main.f85b885f.chunk.css"
+  },
+  {
+    "revision": "ae803663aad3280af5aa",
+    "url": "/movie_app_2020/static/js/2.7790d381.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2020/static/js/2.7790d381.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098fa475eabab2caaaf9",
+    "url": "/movie_app_2020/static/js/main.d62fedb6.chunk.js"
+  },
+  {
+    "revision": "21664dc3e2d58fbc7515",
+    "url": "/movie_app_2020/static/js/runtime-main.de86b078.js"
+  }
+]);
